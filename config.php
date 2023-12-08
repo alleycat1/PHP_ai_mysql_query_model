211ebf228215d6cf85edf46c8c95be4e7bd0cfb5
alleycat1 <netragonlong@gmail.com>
2023-12-08 11:49:37 +0900

    removed open key

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
  
-define('API_KEY', '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'); 
+define('API_KEY', 'KEY'); 
 define('API_ENDPOINT', 'https://api.openai.com/v1/chat/completions');
 
 // Database configuration  
